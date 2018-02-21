2b70c2cfe0fb44d424f92d500106d403a59d96ad
PamelaDiazSerrano <36692764+PamelaDiazSerrano@users.noreply.github.com>
2018-02-21 09:46:15 +0100

    Calculo del área de un cuadrado introduciendo altura y base
    Pamela Diaz

---
+++ AreaCuadrado.c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+	int altura, base, area;
+	printf("Introduce altura\n");
+	scanf_s("%d", &altura);
+	printf("Introduce base\n");
+	scanf_s("%d", &base);
+	printf("El area es: %d\n", (altura*base));
+	system("pause");
+
+
+}
