00c1614aa6087955b8e4a4d7511df23032695455
Alejandrolopezguerrero <36694414+Alejandrolopezguerrero@users.noreply.github.com>
2018-02-21 10:32:39 +0100

    Conversor_monetario

---
+++ Conversor_monetario.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+	float euros, pesetas, dollar;
+	printf("\n Bienvenido al programa \"Conversor monetario\"\n\n A continuacion introduzca el valor en Euros: ");
+	scanf_s("%f", &euros);//Lectura de la variable euros aportada por el usuario
+	
+	pesetas = euros * 166.66;//Conversion
+	dollar = euros * 1.24363;
+	printf("\n\n\tEuros:   %.2f €\n\n\tPesetas: %.2f pts\n\n\tDolares: %.2f $\n\n", euros, pesetas, dollar);
+	system("PAUSE()");
+}
